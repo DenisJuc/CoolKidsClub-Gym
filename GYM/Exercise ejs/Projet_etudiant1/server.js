75dc6ce7d41c267b10f907afbfa0db7c05116e52
DDabula <115166864+DDabula@users.noreply.github.com>
2024-02-12 11:57:29 -0500

    placer items navbar

--- GYM/Exercise ejs/Projet_etudiant1/server.js
+++ GYM/Exercise ejs/Projet_etudiant1/server.js
@@ -46,3 +46,86 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("connected!");
 });
+/*
+    Description des routes
+*/
+app.get("/", function (req, res) {
+    con.query("SELECT * FROM e_events ORDER BY e_start_date DESC", function (err, result) {
+        if (err) throw err;
+        res.render("pages/index", {
+          siteTitle: "Application simple",
+          pageTitle: "Liste d'événements",
+          items: result
+        });
+    });
+});
+
+app.get("/event/add", function (req, res) {
+    con.query("SELECT * FROM e_events ORDER BY e_start_date DESC", function (err, result) {
+        if (err) throw err;
+        res.render("pages/add-event", {
+          siteTitle: "Application simple",
+          pageTitle: "Ajouter un nouvel événement",
+          items: result
+        });
+    });
+});
+app.post("/event/add", function (req, res) {
+    const requete = "INSERT INTO e_events (e_name, e_start_date, e_start_end, e_desc, e_location) VALUES (?, ?, ?, ?, ?)";
+    const parametres = [
+        req.body.e_name,
+        dateFormat(req.body.e_start_date, "yyyy-mm-dd"),
+        dateFormat(req.body.e_start_end, "yyyy-mm-dd"),
+        req.body.e_desc,
+        req.body.e_location
+    ];
+    con.query(requete, parametres, function (err, result) {
+        if (err) throw err;
+        res.redirect("/");
+    });
+});
+/*
+    Permettre l'utilisation de body lors des POST request
+*/
+
+app.get("/event/edit/:id", function (req, res) {
+    const requete = "SELECT * FROM e_events WHERE e_id = ?";
+    const parametres = [req.params.id];
+    con.query(requete, parametres, function (err, result) {
+      if (err) throw err;
+      result[0].E_START_DATE = dateFormat(result[0].E_START_DATE, "yyyy-mm-dd");
+      result[0].E_START_END = dateFormat(result[0].E_START_END, "yyyy-mm-dd");
+      res.render("pages/edit-event.ejs", {
+        siteTitle: "Application simple",
+        pageTitle: "Editer événement : " + result[0].e_name,
+        items: result,
+      });
+    });
+});
+app.post("/event/edit/:id", function (req, res) {
+    const requete = "UPDATE e_events SET e_name = ?, e_start_date = ?, e_start_end = ?, e_desc = ?, e_location = ? WHERE e_id = ?";
+    const parametres = [
+        req.body.e_name,
+        req.body.e_start_date,
+        req.body.e_end_date,
+        req.body.e_desc,
+        req.body.e_location,
+        req.body.e_id
+    ];
+    console.log(parametres);
+    con.query(requete, parametres, function (err, result) {
+        if (err) throw err;
+        res.redirect("/");
+    });
+});
+app.get("/event/delete/:id", function (req, res) {
+    const requete = "DELETE FROM e_events WHERE e_id = ?";
+    con.query(requete, [req.params.id], function (err, result) {
+        if (err) throw err;
+        res.redirect("/");
+    });
+});
+
+
+
+
